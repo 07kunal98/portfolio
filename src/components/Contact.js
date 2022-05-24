d9d4aa4fe993a36e236a36e0fa4a12b758d909f3
Kunal Solanki <07kunal98@gmail.com>
2022-05-25 01:20:12 +0530

    Fixing netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ export default function Contact() {
       )
       .join("&");
   }
-  
+
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/", {
@@ -22,12 +22,14 @@ export default function Contact() {
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
+
+      e.preventDefault();
   }
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
             <form
-          netlify
+          method="POST" data-netlify="true"
           name="contact"
           onSubmit={handleSubmit}
           className="flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -45,6 +47,7 @@ export default function Contact() {
               type="text"
               id="name"
               name="name"
+              value={name}
               onChange={(e) => setName(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-400 focus:ring-indigo-800 focus:ring-2  text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
@@ -57,6 +60,7 @@ export default function Contact() {
               type="email"
               id="email"
               name="email"
+              value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-400 focus:ring-2 focus:ring-indigo-800 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
@@ -64,12 +68,14 @@ export default function Contact() {
           <div className="relative mb-4">
             <label
               htmlFor="message"
+              
               className="leading-7 text-sm text-gray-400">
               Message
             </label>
             <textarea
               id="message"
               name="message"
+              value={message}
               onChange={(e) => setMessage(e.target.value)}
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
@@ -79,6 +85,7 @@ export default function Contact() {
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
+          <input type="hidden" name="form-name" value="contact" />
         </form>
       </div>
     </section>
