a7988f8c2fdccc18858fe22f3c36489ec7b92695
Kunal Solanki <07kunal98@gmail.com>
2022-05-25 16:34:23 +0530

    removed contact js functions

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,32 +4,13 @@ export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
-
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
-  }
+  
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
             <form
           action="POST" data-netlify="true"
           name="contact"
-          onSubmit={handleSubmit}
           className="flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-gray-800 sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me 
